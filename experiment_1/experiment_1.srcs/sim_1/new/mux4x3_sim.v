1e2491d93433ee7e566f7ba76c06985c593bbce4
sckrtge <2962886557@qq.com>
2023-10-09 14:23:06 +0800

    下载到开发板
    修改了一些bug，成功在开发板上面运行

--- experiment_1/experiment_1.srcs/sim_1/new/mux4x3_sim.v
+++ experiment_1/experiment_1.srcs/sim_1/new/mux4x3_sim.v
@@ -27,19 +27,19 @@ module mux4x3_sim(
     reg [2:0] s1 = 3'b111;
     reg [2:0] s2 = 3'b010;
     reg [2:0] s3 = 3'b110;
-    reg [2:0] s14 = 3'b000;
-    reg [2:0] s15 = 3'b000;
+    reg [0:0] s14 = 0;
+    reg [0:0] s15 = 0;
     wire [2:0] led;
     
-    mux4x3 u(.s0(s0), .s1(s1), .s2(s2), .s3(s3), .s14(s14), .s15(s15), .led(led));
+    mux4x3_verilog u(.s0(s0), .s1(s1), .s2(s2), .s3(s3), .s14(s14), .s15(s15), .led(led));
     
     initial begin
     #100 s0 = 3'b001;
-    #100 s14 = 3'b111;
+    #100 s14 = 1;
     #100 s1 = 3'b101;
-    #100 s15 = 3'b111;
+    #100 s15 = 1;
     #100 s3 = 3'b011;
-    #100 s14 = 3'b000;
+    #100 s14 = 0;
     #100 s2 = 3'b100;
     end
 endmodule
--- experiment_1/experiment_1.srcs/sources_1/new/mux4x3_verilog.v
+++ experiment_1/experiment_1.srcs/sources_1/new/mux4x3_verilog.v
@@ -25,9 +25,17 @@ module mux4x3_verilog(
         input [2:0] s1,
         input [2:0] s2,
         input [2:0] s3,
-        input [2:0] s14,
-        input [2:0] s15,
+        input [0:0] s14,
+        input [0:0] s15,
         output [2:0] led
     );
-    assign led = (~s15&~s14&s0)|(~s15&s14&s1)|(s15&~s14&s2)|(s15&s14&s3);
+    wire [2:0] s4;
+    wire [2:0] s5;
+    assign s4[2] = s14[0];
+    assign s4[1] = s14[0];
+    assign s4[0] = s14[0];
+    assign s5[2] = s15[0];
+    assign s5[1] = s15[0];
+    assign s5[0] = s15[0];
+    assign led = (~s5&~s4&s0)|(~s5&s4&s1)|(s5&~s4&s2)|(s5&s4&s3);
 endmodule
